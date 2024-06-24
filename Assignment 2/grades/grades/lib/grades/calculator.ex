2c6462767f35c8533975e8b5147ca9b35450e6de
Catherine <catherine.lee.le@gmail.com>
2024-06-24 19:44:36 -0400

    extract fail_to_participate

--- Assignment 2/grades/grades/lib/grades/calculator.ex
+++ Assignment 2/grades/grades/lib/grades/calculator.ex
@@ -38,7 +38,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
@@ -71,7 +71,7 @@ defmodule Grades.Calculator do
       |> Enum.reject(fn mark -> mark < 0.25 end)
       |> Enum.count()
 
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
       mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
