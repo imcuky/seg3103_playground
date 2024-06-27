727ef405c4f41d3d91c40b2a464796036fa230e1
Catherine <catherine.lee.le@gmail.com>
2024-06-27 17:24:09 -0400

    2 additional refactoring

--- Assignment 2/grades/grades/lib/grades/calculator.ex
+++ Assignment 2/grades/grades/lib/grades/calculator.ex
@@ -17,6 +17,47 @@ defmodule Grades.Calculator do
     0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
   end
 
+  def calculate_mark_boundaries(mark) do
+
+    cond do
+        mark > 0.895 -> {10, "A+"}
+        mark > 0.845 -> {9, "A"}
+        mark > 0.795 -> {8, "A-"}
+        mark > 0.745 -> {7, "B+"}
+        mark > 0.695 -> {6, "B"}
+        mark > 0.645 -> {5, "C+"}
+        mark > 0.595 -> {4, "C"}
+        mark > 0.545 -> {3, "D+"}
+        mark > 0.495 -> {2, "D"}
+        mark > 0.395 -> {1, "E"}
+        :else -> {0, "F"}
+      end
+  end
+
+  def calculate_participate_condition(homework, labs, midterm, final) do
+
+    avg_homework = avg(homework)
+
+    avg_labs = avg(labs)
+
+    avg_exams = (midterm + final) / 2
+
+    num_labs =
+      labs
+      |> Enum.reject(fn mark -> mark < 0.25 end)
+      |> Enum.count()
+
+    {avg_homework, avg_labs, avg_exams, num_labs}
+
+  end
+
+
+
+
+
+
+
+
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
     avg_homework = avg(homework)
 
@@ -27,68 +68,30 @@ defmodule Grades.Calculator do
   end
 
   def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework = avg(homework)
-
-    avg_labs = avg(labs)
-
-    avg_exams = (midterm + final) / 2
-
-    num_labs =
-      labs
-      |> Enum.reject(fn mark -> mark < 0.25 end)
-      |> Enum.count()
+    
+    {avg_homework, avg_labs, avg_exams, num_labs} = calculate_participate_condition(homework, labs, midterm, final)
 
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
-      cond do
-        mark > 0.895 -> "A+"
-        mark > 0.845 -> "A"
-        mark > 0.795 -> "A-"
-        mark > 0.745 -> "B+"
-        mark > 0.695 -> "B"
-        mark > 0.645 -> "C+"
-        mark > 0.595 -> "C"
-        mark > 0.545 -> "D+"
-        mark > 0.495 -> "D"
-        mark > 0.395 -> "E"
-        :else -> "F"
-      end
+      calculate_mark_boundaries(mark) |> elem(1)
     end
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework = avg(homework)
+    
+    {avg_homework, avg_labs, avg_exams, num_labs} = calculate_participate_condition(homework, labs, midterm, final)
 
-    avg_labs = avg(labs)
-
-    avg_exams = (midterm + final) / 2
-
-    num_labs =
-      labs
-      |> Enum.reject(fn mark -> mark < 0.25 end)
-      |> Enum.count()
 
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
-      cond do
-        mark > 0.895 -> 10
-        mark > 0.845 -> 9
-        mark > 0.795 -> 8
-        mark > 0.745 -> 7
-        mark > 0.695 -> 6
-        mark > 0.645 -> 5
-        mark > 0.595 -> 4
-        mark > 0.545 -> 3
-        mark > 0.495 -> 2
-        mark > 0.395 -> 1
-        :else -> 0
-      end
+      calculate_mark_boundaries(mark) |> elem(0)
+      
     end
   end
 end
